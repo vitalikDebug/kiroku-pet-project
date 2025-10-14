fedd621ed49e565eac5d457da0227094d49e18ab
Vitalik <gghjbhgfbb@gmail.com>
2025-10-14 22:52:37 +0300

    change apiRequest

--- client/src/services/apiRequest.js
+++ client/src/services/apiRequest.js
@@ -1,8 +1,10 @@
 import axios from "axios"
 
 const apiRequest = axios.create({
-    baseURL: "http://localhost:4444/api/",
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:4444/api/",
     withCredentials: true
 })
+
+console.log('API Base URL:', import.meta.env.VITE_API_URL)
 
 export default apiRequest;
